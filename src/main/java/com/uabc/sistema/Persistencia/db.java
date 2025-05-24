b276d8317022ff0b2c74f5170e211fe4c8487504
eduardo-grim <bautista.quintero@uabc.edu.mx>
2025-05-23 23:27:40 -0700

    PBI_ADMIN_US

--- src/main/java/com/uabc/sistema/Persistencia/db.java
+++ src/main/java/com/uabc/sistema/Persistencia/db.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class db {
     private static final String URL = "jdbc:mysql://localhost:3306/sistema_notificaciones"; // <- tu nombre de base aquí
     private static final String USER = "root";
-    private static final String PASSWORD = "admin";
+    private static final String PASSWORD = "root";
 
     public static Connection conectar() throws SQLException {
         try {
